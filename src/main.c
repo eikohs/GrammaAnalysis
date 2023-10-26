e31c0f6ab70950d99c1c00600c26ff7f364b73f7
eikohs <cwz_eikoh@163.com>
2023-10-26 00:13:37 -0700

    生成CMake文件

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <../include/Gramma.h>
+
+int main(int argc, char * argv[]){
+    if(argc < 2){
+        puts("No arguments!");
+    }
+    else{
+        printf("Argument1 : %s", argv[1]);
+    }
+}
